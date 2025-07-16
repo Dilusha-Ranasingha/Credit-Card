3e5b023ff6caa6887fa998e63abbd91d30204309
Dilusha-Ranasingha <dilushar10@gmail.com>
2025-07-16 12:04:23 +0530

    feat: Add routes for ApplicationForm02 to ApplicationForm07 components

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,12 @@ import CheckSrilankanEligibility from './pages/CheckSrilankanEligibility';
 import ExistCustomerDetailCheck from './pages/ExistCustomerDetailCheck';
 import CreditLimitSection from './pages/CreditLimitSection';
 import ApplicationForm01 from './pages/ApplicationForm01';
+import ApplicationForm02 from './pages/ApplicationForm02';
+import ApplicationForm03 from './pages/ApplicationForm03';
+import ApplicationForm04 from './pages/ApplicationForm04';
+import ApplicationForm05 from './pages/ApplicationForm05';
+import ApplicationForm06 from './pages/ApplicationForm06';
+import ApplicationForm07 from './pages/ApplicationForm07';
 
 function App() {
   return (
@@ -26,6 +32,12 @@ function App() {
               <Route path="/exist-customer-detail" element={<ExistCustomerDetailCheck />} />
               <Route path="/credit-limit-select" element={<CreditLimitSection />} />
               <Route path="/application-form-01" element={<ApplicationForm01 />} />
+              <Route path="/application-form-02" element={<ApplicationForm02 />} />
+              <Route path="/application-form-03" element={<ApplicationForm03 />} />
+              <Route path="/application-form-04" element={<ApplicationForm04 />} />
+              <Route path="/application-form-05" element={<ApplicationForm05 />} />
+              <Route path="/application-form-06" element={<ApplicationForm06 />} />
+              <Route path="/application-form-07" element={<ApplicationForm07 />} />
             </Routes>
           </main>
         <Footer />
